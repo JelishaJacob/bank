9dfb5142e65627a6f16e12cffcc402d65d907ddf
unknown <jelishajacob@gmail.com>
2023-08-04 15:19:35 +0530

    bank

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  data1="Happy Banking with SBI"
+  data2="Enter Account No."
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  login(){
+    alert("Welcome to SBI")
+  }
+  
+  acChange(event:any){
+    console.log(event.target.value); 
+  }
+
+  pwdChange(event:any){
+    console.log(event.target.value);
+  }
+
+}
